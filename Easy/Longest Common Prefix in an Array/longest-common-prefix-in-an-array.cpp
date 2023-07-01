ef79c74d22f2f569d6e2fe4165d032a0c98f15ad
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2023-07-01 14:29:33 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Longest Common Prefix in an Array/longest-common-prefix-in-an-array.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    
+    string longestCommonPrefix (string arr[], int N)
+    {
+        string ans="";
+        int check=0;
+        int min_s=1e3+1;
+        for(int i=0;i<N;i++){
+            if(min_s>arr[i].size()) min_s=arr[i].size();
+        }
+        for(int i=0;i<min_s;i++){
+            for(int j=0;j<N-1;j++){
+                if(arr[j][i]!=arr[j+1][i]){
+                    check=1;
+                    break;
+                } 
+            }
+            if(check) break;
+            else ans+=arr[0][i];
+        }
+        if(ans.size()==0) ans+="-1";
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n; cin >> n;
+        string arr[n];
+        for (int i = 0; i < n; ++i)
+            cin >> arr[i];
+        
+        Solution ob;
+        cout << ob.longestCommonPrefix (arr, n) << endl;
+    }
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
