0efb57593fe47006feaf08660723d9eefa7b063a
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2023-07-10 15:06:59 +0530

    Time: 8 ms (81.82%), Space: 6.1 MB (29.89%) - LeetHub

--- 0767-prime-number-of-set-bits-in-binary-representation/0767-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0767-prime-number-of-set-bits-in-binary-representation/0767-prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int countPrimeSetBits(int left, int right) {
-        vector <int> primes(1e6+1);
-        iota(primes.begin(),primes.end(),0);
+        vector <int> primes(33);
         primes[1]=0;
-        for(int i=2;i*i<=primes.size()-1;i++){
-            if(primes[i]!=0)
-                for (int j = i * i; j <= primes.size()-1; j += i)
-                    primes[j] = 0;
-        }
+        primes[2]=1;
+        primes[3]=1;
+        primes[5]=1;
+        primes[7]=1;
+        primes[11]=1;
+        primes[13]=1;
+        primes[17]=1;
+        primes[19]=1;
+        primes[23]=1;
+        primes[29]=1;
+        primes[31]=1;
         int ans=0;
         for(int i=left;i<=right;i++){
             int x = __builtin_popcount(i);
