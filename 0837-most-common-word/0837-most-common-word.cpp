8b5a5b18265e8d7e413d864c4c74cd0f36ded3d1
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2023-07-12 00:02:33 +0530

    Time: 3 ms (88.49%), Space: 7.5 MB (99.51%) - LeetHub

--- 0837-most-common-word/0837-most-common-word.cpp
+++ 0837-most-common-word/0837-most-common-word.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     string mostCommonWord(string paragraph, vector<string>& banned) {
-        unordered_map <string,int> m1,m2;
-        for(int i=0;i<banned.size();i++){
-            m1[banned[i]]++;
-        }
+        unordered_map <string,int> m2;
         stringstream ss(paragraph);
         string word;
         while(ss>>word){
             string actual="";
             for(int i=0;i<word.size();i++){
                 if(word[i]=='!' || word[i]=='?' || word[i]=='\'' || word[i]==',' || word[i]==';'|| word[i]=='.'){
-                if(actual.size()>0 && m1.find(actual)==m1.end()){
+                if(actual.size()>0){
                 m2[actual]++;
                 actual="";
             }  
                 }
                 else actual+=tolower(word[i]);
             }
-            if(actual.size()>0 && m1.find(actual)==m1.end()){
+            if(actual.size()>0){
                 m2[actual]++;
             }
         }
         int maxi=INT_MIN;
         string ans="";
+        for(auto s:banned){
+            m2.erase(s);
+        }
         for(auto &pr:m2){
             if(pr.second>maxi){
                 maxi=pr.second;
