c34e289a2cdf17042d3e91a3ddbe7a3346c9ee0a
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2023-07-22 21:47:21 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Remove duplicates from an unsorted linked list/remove-duplicates-from-an-unsorted-linked-list.cpp
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+void print(Node *root)
+{
+Node *temp = root;
+while(temp!=NULL)
+{
+cout<<temp->data<<" ";
+temp=temp->next;
+}
+}
+
+
+// } Driver Code Ends
+/*
+The structure of linked list is the following
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+
+class Solution
+{
+    public:
+    //Function to remove duplicates from unsorted linked list.
+    Node * removeDuplicates( Node *head) 
+    {
+        unordered_map <int,int> m,vis;
+        struct Node *ptr = head;
+        while(ptr!=NULL){
+            m[ptr->data]++;
+            vis[ptr->data]=0;
+            ptr=ptr->next;
+        }
+        ptr=head;
+        vis[ptr->data]=1;
+        while(ptr!=NULL && ptr->next!=NULL){
+            if(m[(ptr->next)->data]>1 && vis[(ptr->next)->data]==1){
+                //delete ptr->next
+                ptr->next=(ptr->next)->next;
+                continue;
+            }
+            else if(m[ptr->next->data]>1){
+                vis[ptr->next->data]=1;
+            }
+            ptr=ptr->next;
+        }
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+ 
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+	    Solution ob;
+		Node *result  = ob.removeDuplicates(head);
+		print(result);
+		cout<<endl;
+		
+	}
+	return 0;
+}
+// } Driver Code Ends
