96cb07b8955e366ddbed89901fdd247f10de9a4a
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2023-07-23 23:23:26 +0530

    C++ Solution
                Submission Statistics:
                Question #: 1708
                Language: C++
                Runtime: 77 ms
                Runtime percentile: 8.91
                Memory: 32.964 MB
                Memory percentile: 80.72

---
+++ 1708. Design Parking System/Solution.cpp
@@ -0,0 +1,24 @@
+class ParkingSystem {
+public:
+    vector <int> v;
+    ParkingSystem(int big, int medium, int small) {
+        v.resize(3);
+        v[0]=big;
+        v[1]=medium;
+        v[2]=small;
+    }
+    
+    bool addCar(int carType) {
+        if(v[carType-1]){
+            v[carType-1]--;
+            return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
