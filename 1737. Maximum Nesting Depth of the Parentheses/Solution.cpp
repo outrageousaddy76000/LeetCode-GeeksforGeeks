0bd1b5a4a008b2a5740dad70506a92fe9a3d5928
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2023-07-23 23:38:12 +0530

    C++ Solution
                Submission Statistics:
                Question #: 1737
                Language: C++
                Runtime: 5 ms
                Runtime percentile: 5.99
                Memory: 6.42 MB
                Memory percentile: 16.34

---
+++ 1737. Maximum Nesting Depth of the Parentheses/Solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        stack <char> st;
+        int ans=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(') st.push(s[i]);
+            else if(s[i]==')') st.pop();
+            ans=max(ans,(int)(st.size()));
+        }
+        return ans;
+    }
+};
