a2a5465338e11b0b300875143853185a60e6147c
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2023-07-23 14:51:39 +0530

    C++ Solution
                Submission Statistics:
                Question #: 1293
                Language: C++
                Runtime: 4 ms
                Runtime percentile: 72.36
                Memory: 8.248 MB
                Memory percentile: 29.34

--- 1293. Three Consecutive Odds/Solution.cpp
+++ 1293. Three Consecutive Odds/Solution.cpp
@@ -3,7 +3,7 @@ public:
     bool threeConsecutiveOdds(vector<int>& arr) {
         if(arr.size()<3) return false;
         for(int i=0;i<arr.size()-2;i++){
-            if(arr[i]%2!=0 && arr[i+1]%2!=0 && arr[i+2]%2!=0) return true;
+            if(arr[i]&1!=0 && arr[i+1]&1!=0 && arr[i+2]&1!=0) return true;
         }
         return false;
     }
