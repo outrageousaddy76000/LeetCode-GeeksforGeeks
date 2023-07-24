2289c97a6555e9ec6c17f35ea381d19e56bed50d
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2023-07-24 23:28:01 +0530

    C++ Solution
                Submission Statistics:
                Question #: 1802
                Language: C++
                Runtime: 3 ms
                Runtime percentile: 65.70
                Memory: 8.588 MB
                Memory percentile: 97.94

---
+++ 1802. Number of Students Unable to Eat Lunch/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int n = students.size();
+        int cnt1 = count(students.begin(),students.end(),0);
+        int cnt2 = n-cnt1;
+        for(int i=0;i<sandwiches.size();i++){
+            if(sandwiches[i]==0){
+                cnt1--;
+            }
+            else{
+                cnt2--;
+            }
+            if(cnt1<0 || cnt2<0) return sandwiches.size()-i;
+        }
+        return 0;
+    }
+};
