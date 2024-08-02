ba07bc69d825784454f71579966532b41a52001a
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2024-08-02 23:57:31 +0530

    Time: 395 ms (13.92%), Space: 73.8 MB (17.95%) - LeetHub

--- 1308-smallest-string-with-swaps/1308-smallest-string-with-swaps.cpp
+++ 1308-smallest-string-with-swaps/1308-smallest-string-with-swaps.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    map <int,vector<char>> chars;
-    map <int,vector<int>> indexes;
-    void dfs(int i,vector <int> &vis,string &s,vector <vector <int>> &adj, int index){
+    void dfs(int i,vector <int> &vis,string &s,vector <vector <int>> &adj,vector <int> &indexes, vector <char> &chars){
         vis[i]=true;
-        chars[index].push_back(s[i]);
-        indexes[index].push_back(i);
+        chars.push_back(s[i]);
+        indexes.push_back(i);
         for(auto &val:adj[i]){
-            if(!vis[val]) dfs(val,vis,s,adj,index);
+            if(!vis[val]) dfs(val,vis,s,adj,indexes,chars);
         }
     }
     string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
         //solution 2 - DFS
-        chars.clear();
-        indexes.clear();
         vector <vector <int>> adj(s.size());
         for(auto &pair:pairs){
             adj[pair[0]].push_back(pair[1]);
@@ -22,18 +18,17 @@ public:
         vector <int> vis(s.size());
         for(int i=0;i<s.size();i++){
             if(!vis[i]){
-                dfs(i,vis,s,adj,i);
-            }
-        }
-        for(auto &pr:chars) sort(pr.second.rbegin(),pr.second.rend());
-        for(auto &pr:indexes) sort(pr.second.rbegin(),pr.second.rend());
-        for(auto &pr:chars){
-            vector <int> ind = indexes[pr.first];
-            vector <char> ch = pr.second;
-            while(ind.size()){
-                s[ind[ind.size()-1]]=ch[ind.size()-1];
-                ind.pop_back();
-                ch.pop_back();
+                vector <int> indexes;
+                vector <char> chars;
+                dfs(i,vis,s,adj,indexes, chars);
+                //dfs complete, operate here only
+                sort(indexes.rbegin(),indexes.rend());
+                sort(chars.rbegin(),chars.rend());
+                while(indexes.size()){
+                    s[indexes[indexes.size()-1]]=chars[indexes.size()-1];
+                    chars.pop_back();
+                    indexes.pop_back();
+                }
             }
         }
         return s;
