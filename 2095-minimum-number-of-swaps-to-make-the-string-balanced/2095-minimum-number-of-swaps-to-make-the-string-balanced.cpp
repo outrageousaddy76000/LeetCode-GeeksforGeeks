848a2d7efa1a481500025a623554610a22a87c0b
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2024-08-03 15:07:12 +0530

    Time: 90 ms (93.5%), Space: 32.5 MB (61.62%) - LeetHub

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+        //two pointer
+        int ans=0;
+        //swap first anomaly with last anomaly
+        int n = s.size();
+        int i=0,j=n-1;
+        int cnt=0,cnt2=0;
+        while(true){
+            while(i<n){
+                if(s[i]=='[') cnt++;
+                else cnt--;
+                if(cnt<0) break;
+                i++;
+            }
+            while(j>=i){
+                if(s[j]==']') cnt2++;
+                else cnt2--;
+                if(cnt2<0) break;
+                j--;
+            }
+            if(j<=i) break;
+            swap(s[i],s[j]);
+            ans++;
+            i++;j--;
+            cnt=1;
+            cnt2=1;
+        }
+        cout<<s<<endl;
+        return ans;
+    }
+};
