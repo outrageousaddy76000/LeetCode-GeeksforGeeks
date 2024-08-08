aa429762f652fffbd8cdcf6e8fbd9179144a83dd
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2024-08-08 12:05:11 +0530

    Time: 100 ms (77.8%), Space: 91.9 MB (40.64%) - LeetHub

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/3249-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/3249-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        vector <int> bitindex;
+        int cnt=0;
         int ans=0;
         for(auto val:nums){
             ans=ans^val;
@@ -9,8 +9,8 @@ public:
         bitset<32> b1(ans);
         bitset<32> b2(k);
         for(int i=0;i<32;i++){
-            if(b1[i]!=b2[i]) bitindex.push_back(i);
+            if(b1[i]!=b2[i]) cnt++;
         }
-        return bitindex.size();
+        return cnt;
     }
 };
