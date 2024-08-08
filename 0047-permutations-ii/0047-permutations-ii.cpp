a41cbbcca1e71fe879dc92a49f5a021299ae7b4b
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2024-08-08 20:28:02 +0530

    Time: 3 ms (87.9%), Space: 10.6 MB (81.93%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        set <vector<int>> ans;
+        do{
+            ans.insert(nums);
+        }while(next_permutation(nums.begin(),nums.end()));
+        vector <vector<int>> res(ans.begin(),ans.end());
+        return res;
+    }
+};
