251047386003e42b345339a90eff147ed9108792
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2024-08-12 13:39:04 +0530

    Time: 4 ms (28.43%), Space: 7.6 MB (49.15%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -5,7 +5,6 @@ public:
         for(int i=5;i<=n;i+=5){
             int cnt=0;
             int temp = i;
-            //how many times can we divide by 5
             while(temp%5==0){
                 ans++;
                 temp/=5;
