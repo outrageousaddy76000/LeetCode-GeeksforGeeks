2f91994b2657bce8509b951606409ab4c700656a
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2024-08-31 09:17:33 +0530

    Time: 130 ms (40.64%), Space: 70.5 MB (35.7%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
+    double max(double a,double b){
+        if(a>b) return a;
+        return b;
+    }
+    vector <bool> vis;
+    vector <double> dis;
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-
-        // Adjacency list
-        vector<vector<pair<int, double>>> adj(n);
-        for (int i = 0; i < edges.size(); i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            adj[u].push_back({v, succProb[i]});
-            adj[v].push_back({u, succProb[i]});
+        vis.resize(n,false);
+        dis.resize(n,0);
+        dis[start]=1.0;
+        vis[start]=true;
+        //create adjacency list
+        priority_queue<pair<double,int>> pq;
+        vector<vector<pair<int,double>>> adj(n);
+        for(int i=0;i<edges.size();i++){
+            adj[edges[i][0]].push_back(make_pair(edges[i][1],succProb[i]));
+            adj[edges[i][1]].push_back(make_pair(edges[i][0],succProb[i]));
         }
-
-        // ans will be in dist[end]
-        vector<double> dist(n, 0.0);
-        dist[start] = 1.0;
-        
-        queue<int> q;
-        q.push(start);
-        
-        while (!q.empty()) {
-            int curr = q.front();
-            q.pop();
-            
-            for (auto x : adj[curr]) {
-                int node = x.first;
-                double prob = x.second;
-                double newProb = dist[curr] * prob;
-                
-                if (newProb > dist[node]) {
-                    dist[node] = newProb;
-                    q.push(node);
+        int last = start;
+        for(int cnt=0;cnt<n-1;cnt++){
+            //apply dijkstra algo from start node
+            for(auto &v: adj[last]){
+                if(vis[v.first]==false){
+                    dis[v.first]=max(dis[v.first],v.second*dis[last]);
+                    pq.push({dis[v.first],v.first});
                 }
             }
+            //reduce time complexity here
+            if(pq.empty()) break;
+            pair <double,int> p;
+            while(1){
+                p = pq.top();
+                pq.pop();
+                if(vis[p.second]==false) break;
+            }
+            vis[p.second]=true;
+            last=p.second;
+            if(last==end) break;
         }
-        
-        return dist[end];
+        return dis[end];
     }
 };
