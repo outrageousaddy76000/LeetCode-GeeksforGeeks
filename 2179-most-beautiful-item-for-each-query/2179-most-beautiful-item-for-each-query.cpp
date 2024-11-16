dae1faf39bcff4cd022d15b34746dfc2d9360db3
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2024-11-17 02:02:13 +0530

    Time: 77 ms (27.15%), Space: 96.2 MB (49.86%) - LeetHub

--- 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.cpp
+++ 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.cpp
@@ -7,10 +7,7 @@ public:
             items[i][1] = max(items[i][1], items[i - 1][1]);
         }
         for(int i=0;i<queries.size();i++){
-            auto it = upper_bound(items.begin(), items.end(), vector<int>{queries[i], INT_MAX}, 
-                [](const vector<int>& a, const vector<int>& b) {
-                    return a[0] < b[0];
-                });
+            auto it = upper_bound(items.begin(), items.end(), vector<int>{queries[i], INT_MAX});
 
             if (it == items.begin()) {
                 ans[i] = 0;
