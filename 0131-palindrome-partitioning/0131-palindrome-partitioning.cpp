63c021134d3fb5591ced028a07bbafbaf69cf42c
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2024-11-22 18:34:05 +0530

    Time: 441 ms (5.04%), Space: 365.5 MB (5%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    bool isPal(string s){
+        for(int i=0;i<s.size()/2;i++){
+            if(s[i]!=s[s.size()-1-i]) return false;
+        }
+        return true;
+    }
+public:
+    vector<vector<string>> partition(string s) {
+        // cout<<s<<endl;
+        vector<vector<string>> ans;
+        for(int i=0;i<s.size();i++){
+            string curr = s.substr(0,i+1);
+            if(isPal(curr)){
+                vector<vector<string>> next;
+                if(i!=s.size()-1){
+                    next = partition(s.substr(i+1,s.size()-i-1));
+                    for(auto &val:next){
+                        val.insert(val.begin(),curr);
+                    }
+                    for(auto &val:next){
+                        ans.push_back(val);
+                    }
+                }
+                else{
+                    vector<string> temp;
+                    temp.push_back(curr);
+                    ans.push_back(temp);
+                }
+            }
+        }
+        return ans;
+    }
+};
