34c154f522849f7ea4f378d005eff9bd6d5ff8ed
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2024-11-25 01:58:06 +0530

    Time: 54 ms (64.09%), Space: 90 MB (62.75%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    int dp[100001][2][3];
+    int solve(vector <int> &prices, int index,bool buy, int k){
+        if(index==prices.size() || k==0) return 0;
+        if(dp[index][buy][k]!=-1) return dp[index][buy][k];
+        int profit=0;
+        if(buy){
+            //can buy
+            //will buy
+            profit =-1*prices[index]+solve(prices,index+1,false,k);
+            //wont buy
+            profit = max(profit,solve(prices,index+1,true,k));
+        }
+        else{
+            //can sell
+            //will sell
+            profit =prices[index]+solve(prices,index+1,true,k-1);
+            //wont buy
+            profit =max(profit,solve(prices,index+1,false,k));
+        }
+    return dp[index][buy][k]=profit;
+    }
+public:
+    int maxProfit(vector<int>& prices) {
+        int k=2;
+        memset(dp,-1,sizeof(dp));
+        return solve(prices,0,true,k);
+    }
+};
