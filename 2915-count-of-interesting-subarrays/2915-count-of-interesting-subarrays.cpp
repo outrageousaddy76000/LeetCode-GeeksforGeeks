e489e20c3f4c84e03e9e828a53356e11c0997699
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2025-01-24 02:33:57 +0530

    Time: 92 ms (20%), Space: 116 MB (84.29%) - LeetHub

--- 2915-count-of-interesting-subarrays/2915-count-of-interesting-subarrays.cpp
+++ 2915-count-of-interesting-subarrays/2915-count-of-interesting-subarrays.cpp
@@ -3,14 +3,12 @@ public:
     long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
         int n = nums.size();
         int cnt=0;
+        long long ans=0;
+        map <int,int> m;
         for(int i=0;i<n;i++){
             if(nums[i]%modulo==k) cnt++;
             nums[i]=cnt;
             nums[i]%=modulo;
-        }
-        map <int,int> m;
-        long long ans=0;
-        for(int i=0;i<n;i++){
             if(m.find(((nums[i]-k)%modulo + modulo)%modulo)!=m.end()){
                 ans+=m[(((nums[i]-k)%modulo + modulo)%modulo)];
             }
