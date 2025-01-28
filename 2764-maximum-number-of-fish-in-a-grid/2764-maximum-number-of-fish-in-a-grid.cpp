37e10f3861d5835ffd0f6fbb7d1d0f8c0147861a
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2025-01-28 14:38:27 +0530

    Time: 11 ms (51.14%), Space: 96 MB (69.18%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector <vector<int>> directions={{1,0},{0,1},{-1,0},{0,-1}};
     int n,m;
-    int dfs(int x,int y,vector<vector<int>>& grid, vector<vector<bool>> &vis){
+    int dfs(int x,int y,vector<vector<int>>& grid){
         int res=grid[x][y];
-        vis[x][y]=true;
+        grid[x][y]=0;
         for(auto &dir:directions){
             int x_ = x+dir[0];
             int y_ = y+dir[1];
             if(x_<0 || y_<0 || x_>=n || y_>=m) continue;
-            if(!vis[x_][y_] && grid[x_][y_]>0){
-                res+=dfs(x_,y_,grid,vis);
+            if(grid[x_][y_]>0){
+                res+=dfs(x_,y_,grid);
             }
         }
         return res;
@@ -18,12 +18,11 @@ public:
     int findMaxFish(vector<vector<int>>& grid) {
         n = grid.size();
         m = grid[0].size();
-        vector<vector<bool>> vis(n,vector <bool> (m));
         int ans=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j] && grid[i][j]>0){
-                    ans=max(ans,dfs(i,j,grid,vis));
+                if(grid[i][j]>0){
+                    ans=max(ans,dfs(i,j,grid));
                 }
             }
         }
