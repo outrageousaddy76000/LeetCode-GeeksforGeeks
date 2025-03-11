2b4c8b226253e8f799d457c2207a305be536bb76
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2025-03-11 23:29:15 +0530

    Time: 3403 ms (5.72%), Space: 11.2 MB (35.03%) - LeetHub

---
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int i=0,j=0;
+        int n = s.size();
+        int ans=0;
+        vector <int> v(3);
+        while(i<n){
+            //for each i, what will be the answer
+            while((v[0]<=0 || v[1]<=0 || v[2]<=0) && j<n){
+                v[s[j]-'a']++;
+                j++;
+            }
+            if(j==n && (v[0]==0 || v[1] ==0 || v[2]==0) ) break;
+            //increase the ans
+            ans+=n-j+1;
+            for(auto &val:v) cout<<val<<" ";
+            v[s[i]-'a']--;
+            i++;
+            cout<<ans<<endl;
+        }
+        return ans;
+    }
+};
