a8f50ac53e71717be1ad775623b583824ae4b632
Adarsh Anand <85014838+outrageousaddy76000@users.noreply.github.com>
2025-03-30 19:39:24 +0530

    Time: 137 ms (24.88%), Space: 51.4 MB (24.58%) - LeetHub

--- 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.cpp
+++ 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.cpp
@@ -1,41 +1,89 @@
+class DSU {
+private:
+    vector<int> parent, size;
+    int n, m;
+public:
+    DSU(int a, int b) {
+        n = a;  // Rows
+        m = b;  // Columns
+        parent.resize(n * m, -1);
+        size.resize(n * m, 1);
+    }
+
+    int find(int node) {
+        if (parent[node] < 0) return node;
+        return parent[node] = find(parent[node]);  // Path compression
+    }
+
+    bool unionFind(int x1, int x2, int y1, int y2) {
+        int p1 = x1 * m + x2;  // Corrected row-major index
+        int p2 = y1 * m + y2;
+
+        int rootA = find(p1);
+        int rootB = find(p2);
+
+        if (rootA == rootB) return false;  // Already in same set
+
+        if (size[rootA] > size[rootB]) {
+            parent[rootB] = rootA;
+            size[rootA] += size[rootB];
+        } else {
+            parent[rootA] = rootB;
+            size[rootB] += size[rootA];
+        }
+        return true;
+    }
+
+    int getSize() {
+        return size[find(0)];
+    }
+};
+
 class Solution {
 private:
-    vector <vector<int>> dir = {
-        {-1,0},{1,0},{0,1},{0,-1}
-    };
+    vector<vector<int>> dir = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
 public:
     vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
         int m = grid.size();
         int n = grid[0].size();
         int k = queries.size();
-        vector <pair<int,int>> q(k);
-        for(int i=0;i<k;i++){
-            q[i]={queries[i],i};
+
+        vector<pair<int, int>> q(k);
+        for (int i = 0; i < k; i++) {
+            q[i] = {queries[i], i};
         }
-        sort(q.begin(),q.end());
-        vector <int> ans(k);
-        priority_queue <pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>, greater <>> pq;
-        pq.push({grid[0][0],{0,0}});
-        vector<vector<bool>> vis(m,vector<bool> (n,false));
-        vis[0][0]=true;
-        int cnt=0;
-        for(auto [val,index]:q){
-            while(!pq.empty() && pq.top().first<val){
-                //can move forward from the current cell
-                auto [x,i] = pq.top();
+        sort(q.begin(), q.end());
+
+        DSU dsu(m, n);  // Fixed constructor order
+        vector<int> ans(k);
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                pq.push({grid[i][j], {i, j}});
+            }
+        }
+
+        for (auto [val, index] : q) {
+            while (!pq.empty() && pq.top().first < val) {
+                auto [x, i] = pq.top();
                 pq.pop();
                 int row = i.first;
                 int col = i.second;
-                cnt++;
-                for(auto &d:dir){
-                    if(row+d[0]>=m || row+d[0]<0) continue;
-                    if(col+d[1]>=n || col+d[1]<0) continue;
-                    if(vis[row+d[0]][col+d[1]]) continue;
-                    pq.push((make_pair(grid[row+d[0]][col+d[1]],make_pair(row+d[0],col+d[1]))));
-                    vis[row+d[0]][col+d[1]]=true;
+
+                for (auto &d : dir) {
+                    int newRow = row + d[0];
+                    int newCol = col + d[1];
+
+                    if (newRow >= m || newRow < 0) continue;
+                    if (newCol >= n || newCol < 0) continue;
+                    if (grid[newRow][newCol] < val) {
+                        dsu.unionFind(row, col, newRow, newCol);
+                    }
                 }
             }
-            ans[index]=cnt;
+            if(grid[0][0]>=val) ans[index]=0;
+            else ans[index] = dsu.getSize();
         }
         return ans;
     }
